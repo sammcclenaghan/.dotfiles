b285a443ab6740a1f24cec833be1fc1f59bde949
Sam McClenaghan <sam@aream.ca>
2025-12-28 20:51:14 -0700

    revamped neovim config

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,22 +1,30 @@
-require("config.lazy")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
 
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.g.mapleader = ' '
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.pack.add({
+	{src = "https://github.com/ellisonleao/gruvbox.nvim"},
+	{ 
+		src = 'https://github.com/nvim-treesitter/nvim-treesitter', 
+		branch = 'master',
+		build = ':TSUpdate' 
+	},
 })
+
+vim.api.nvim_create_autocmd("FileType", { callback = function() pcall(vim.treesitter.start) end })
+vim.opt.foldmethod, vim.opt.foldexpr = "expr", "v:lua.vim.treesitter.foldexpr()"
+
+-- C) Native Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+
+vim.lsp.enable({"lua_ls"})
+
+vim.cmd("colorscheme gruvbox")
