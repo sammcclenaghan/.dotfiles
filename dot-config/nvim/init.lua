ea12e97f6434a084da836b178f45869172b4984d
Sam McClenaghan <sam@aream.ca>
2026-03-30 00:00:38 -0700

    changed config

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,68 +1,156 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+---@diagnostic disable: missing-fields
 
+-- INFO: introduction
+-- this is a minimal neovim configuration written in lua. this is not meant to
+-- be a distribution, but rather a template for you to build upon and/or a
+-- reference for how to configure neovim using lua in the latest version.
+--
+-- TUTOR:
+-- if you're completely new to neovim and/or vim, consider going through
+-- `:Tutor` inside neovim to get a basic idea of how it works.
+--     if you don't know what this means, type the following:
+--       - <escape key>
+--       - :
+--       - Tutor
+--       - <enter key>
+--
+-- LUA:
+-- some level of familiarity with lua/programming languages are also expected.
+-- if you're new to lua, consider going through the official reference:
+--    https://www.lua.org/manual
+-- or a more friendly tutorial like:
+--    https://learnxinyminutes.com/docs/lua/
+-- you can also check out `:h lua-guide` inside neovim for a neovim-specific
+-- lua guide.
+--
+-- DEPENDENCIES:
+-- this configuration assumes you have the following tools installed on your
+-- system:
+--    `git` - for vim builtin package manager. (see `:h vim.pack`)
+--    `ripgrep` - for fuzzy finding
+--    clipboard tool: xclip/xsel/win32yank - for clipboard sharing between OS and neovim (see `h: clipboard-tool`)
+--    a nerdfont (ensure the terminal running neovim is using it)
+-- run `:checkhealth` inside neovim to see if your system is missing anything.
+--
+-- MINIMAL:
+-- to say that something is 'minimal' you have to define what variable you're
+-- minimizing. this configuration minimizes for lines of code and concepts.
+-- to some, this configuration may have too many plugins. for example, using
+-- mason.nvim to manage lsp servers will be an unnecessary dependency if the
+-- user is already familiar with lsps and is comfortable managing them through
+-- their OS package manager. but to someone that isn't familiar with lsp servers
+-- this approach wouldn't cover everything needed to have the 'minimum' necessary
+-- for lsp + completion + fuzzy finding. to some, fuzzy finding is also a bloated
+-- dependency.
+-- this configuration is only a starting point/reference. it is expected that
+-- the user will change the configuration to suit their needs.
+
+
+-- INFO: options
+-- these change the default neovim behaviours using the 'vim.opt' API.
+-- see `:h vim.opt` for more details.
+-- run `:h '{option_name}'` to see what they do and what values they can take.
+-- for example, `:h 'number'` for `vim.opt.number`.
+
+-- set <space> as the leader key
+-- must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- enable true color support
+vim.opt.termguicolors = true
+
+-- make line numbers default
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus'
+-- enable mouse mode, can be useful for resizing splits
+vim.opt.mouse = "a"
 
-vim.opt.signcolumn = 'yes'
+-- sync clipboard between OS and neovim.
+--  remove this option if you want your OS clipboard to remain independent.
+--  see `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
 
+-- save undo history
+vim.opt.undofile = true
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- enable live preview of substitutions
+vim.opt.inccommand = "split"
+
+-- show which line your cursor is on
 vim.opt.cursorline = true
+
+-- set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
+
+-- enable break indent
 vim.opt.breakindent = true
+
+-- enable line wrapping
 vim.opt.wrap = true
 
+-- formatting
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.textwidth = 80
 
 vim.diagnostic.config({
-  virtual_text = true, 
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+    },
+  },
+  virtual_text = true, -- show inline diagnostics
 })
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- clear search highlights with <Esc>
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.pack.add({ "https://github.com/ellisonleao/gruvbox.nvim" })
+-- INFO: colorscheme
+vim.cmd.colorscheme("gruvbox")
 
-vim.cmd.colorscheme('gruvbox')
+-- INFO: plugins
+-- we install plugins with neovim's builtin package manager: vim.pack
+-- and then enable/configure them by calling their setup functions.
+--
+-- (see `:h vim.pack` for more details on how it works)
+-- you can press `gx` on any of the plugin urls below to open them in your
+-- browser and check out their documentation and functionality.
+-- alternatively, you can run `:h {plugin-name}` to read their documentation.
+--
+-- plugins are then loaded and configured with a call to `setup` functions
+-- provided by each plugin. this is not a rule of neovim but rather a convention
+-- followed by the community.
+-- these setup calls take a table as an agument and their expected contents can
+-- vary wildly. refer to each plugin's documentation for details.
 
+-- INFO: formatting and syntax highlighting
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" }, { confirm = false })
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" })
 
+-- equivalent to :TSUpdate
 require("nvim-treesitter.install").update("all")
-require("nvim-treesitter.configs").setup({
+
+require("nvim-treesitter").setup({
   auto_install = true, -- autoinstall languages that are not installed yet
 })
 
+-- INFO: keybinding helper
+vim.pack.add({ "https://github.com/folke/which-key.nvim" }, { confirm = false })
 
-vim.pack.add({
-  { src = 'https://github.com/ellisonleao/gruvbox.nvim' },
-  {
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-    data = {
-      run = function(_) vim.cmd 'TSUpdate' end,
-    },
-  },
-  { src = 'https://github.com/echasnovski/mini.pick' },
-  { src = 'https://github.com/echasnovski/mini.surround' },
-  { src = 'https://github.com/stevearc/oil.nvim' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects' },
-  { src = 'https://github.com/folke/which-key.nvim' },
+require("which-key").setup({
+  spec = {
+    { "<leader>s", group = "[S]earch", icon = { icon = "", color = "green", }, },
+  }
 })
 
-require('mini.pick').setup()
-vim.keymap.set('n', '<leader>ff', MiniPick.builtin.files, { desc = 'Find files' })
-vim.keymap.set('n', '<leader>fg', MiniPick.builtin.grep_live, { desc = 'Live grep' })
-vim.keymap.set('n', '<leader>fb', MiniPick.builtin.buffers, { desc = 'Buffers' })
-
-require('which-key').setup()
-require('mini.surround').setup()
-
-require('oil').setup()
-vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Open parent directory' })
-
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
-})
+-- uncomment to enable automatic plugin updates
+-- vim.pack.update()
